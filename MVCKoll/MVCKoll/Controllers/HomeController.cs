bc97fefd2a1e6d522796f53a6c56596ec7a3e2c9
CashmereBob <weidmar@gmail.com>
2016-11-28 20:46:19 +0100

    First commit

---
+++ MVCKoll/MVCKoll/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKoll.Models;
+
+namespace MVCKoll.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<AdressViewModel> adresser = new List<AdressViewModel>();
+        // GET: Adress
+        public ActionResult Index()
+        {
+            return View(adresser);
+        }
+
+        // GET: Adress/Create
+        public ActionResult Create()
+        {
+            return PartialView("_addAdress");
+        }
+
+        // POST: Adress/Create
+        [HttpPost]
+        public ActionResult Create(AdressViewModel model)
+        {
+            if (ModelState.IsValid) { 
+            try
+            {
+                adresser.Add(new AdressViewModel {
+                    id = Guid.NewGuid(),
+                    Adress = model.Adress,
+                    Name = model.Name,
+                    Telefonnr = model.Telefonnr,
+                    Date = DateTime.Now
+                });
+
+                return PartialView("_Result", adresser);
+            }
+            catch
+            {
+                return View();
+            }
+            } else
+            {
+                return PartialView("_Result", adresser);
+            }
+        }
+
+        // GET: Adress/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            var adress = adresser.Single(x => x.id == id);
+            return PartialView("_Edit", adress);
+        }
+
+        public ActionResult Cancel(Guid id)
+        {
+            var adress = adresser.Single(x => x.id == id);
+            return PartialView("_Cancel",adress);
+        }
+
+        public ActionResult CancelAdress()
+        {
+            return PartialView("_cancelAdress", adresser);
+        }
+
+        // POST: Adress/Edit/5
+        [HttpPost]
+        public ActionResult Edit(AdressViewModel model)
+        {
+            try
+            {
+                var oldAdress = adresser.Single(x => x.id == model.id);
+                oldAdress.Name = model.Name;
+                oldAdress.Adress = model.Adress;
+                oldAdress.Telefonnr = model.Telefonnr;
+                oldAdress.Date = DateTime.Now;
+
+                return PartialView("_Cancel", oldAdress);
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // POST: Adress/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, FormCollection collection)
+        {
+            try
+            {
+                var oldAdress = adresser.Single(x => x.id == id);
+                adresser.Remove(oldAdress);
+
+                return PartialView("_Result", adresser);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCKoll/MVCKoll/Models/AdressViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKoll.Models
+{
+    public class AdressViewModel
+    {
+        public Guid id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public string Telefonnr{ get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
